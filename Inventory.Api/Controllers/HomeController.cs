a56f042cdf0cd99094a4e1578f1ae462c7f08a54
madstone290 <madstone290@gmail.com>
2022-07-18 18:37:34 +0900

    Update version 1.9.11

--- Inventory.Api/Controllers/HomeController.cs
+++ Inventory.Api/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ namespace Inventory.Api.Controllers
     [Route("/")]
     public class HomeController : ControllerBase
     {
-        private const string Version = "1.7.11";
+        private const string Version = "1.9.11";
 
         [HttpGet]
         public string Get()
